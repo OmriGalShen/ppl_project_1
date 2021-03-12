56b2719df671be9c3f3e9081140eed3f4707c68f
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-03-12 19:55:59 +0200

    Worked on countVowels and runLengthEncoding

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,12 +1,56 @@
 import * as R from "ramda";
 
+/*
+    helper function stringToArray which takes a string and returns an array of the
+    characters that make up the string. For example:
+    stringToArray("Hello!"); // ==> [ 'H', 'e', 'l', 'l', 'o', '!' ]
+*/
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countVowels = undefined;
+/*
+    Write a function countVowels that takes a string as input and returns the number of vowels in the text.
+    Reminder: vowels are one of ‘a’, ‘e’, ‘i’, ‘o’, ‘u’, either uppercase or lowercase. For example:
+    countVowels("This is SOME Text"); // ==> 5 
+*/
+export const countVowels: (str: string) => number = (str: string) =>
+  R.filter(checkVowel, stringToArray(str)).length;
+
+const checkVowel: (ch: string) => boolean = (ch: string) => {
+  const vowelList = ["a", "e", "i", "o", "u"];
+  return vowelList.includes(ch.toLowerCase());
+};
+
+// console.log(countVowels("This is SOME Text"));
 
 /* Question 2 */
-export const runLengthEncoding = undefined;
+/*
+    Write a function runLengthEncoding which takes a string as input and returns a “compressed” version of
+    it, where identical consecutive characters appear as the character followed by its count. For example:
+    runLengthEncoding("aaaabbbccd"); // ==> 'a4b3c2d'
+
+*/
+
+//"aaaabbbccd"==> [ 'aaaa', 'bbb', 'cc', 'd' ]
+const f1: (str: string) => string[] = (str: string) =>
+  R.groupWith(R.equals, str);
+
+//[ 'aaaa', 'bbb', 'cc', 'd' ] ==> [ 'a4', 'b3', 'c2', 'd1' ]
+const f2: (arr: string[]) => string[] = (arr: string[]) =>
+  R.map((x: string) => x.charAt(0) + x.length.toString(), arr);
+
+//[ 'aaaa', 'bbb', 'cc', 'd' ] ==> a4b3c2d1
+const f3: (arr: string[]) => string = (arr: string[]) => R.join("", arr);
+
+export const runLengthEncoding: (str: string) => string = R.pipe(f1, f2, f3);
+
+console.log(runLengthEncoding("aaaabbbccd"));
 
 /* Question 3 */
+/*
+    Write a function isPaired that takes a string and returns whether the parentheses ({, }, (, ), [, ]) in the
+    string are paired. For example:
+    isPaired("This is ([some]) {text}"); // ==> true
+    isPaired("This is ]some[ (text)"); // ==> false
+*/
 export const isPaired = undefined;
