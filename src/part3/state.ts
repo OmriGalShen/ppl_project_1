c883c762a7e22a046287e715b35d2bac7b17bbfd
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-03-22 01:48:23 +0200

    worked on bind

--- src/part3/state.ts
+++ src/part3/state.ts
@@ -1,3 +1,11 @@
 export type State<S, A> = (initialState: S) => [S, A];
 
-export const bind = undefined;
+export const bind: <S, A, B>(
+  state: State<S, A>,
+  f: (x: A) => State<S, B>
+) => State<S, B> = <S, A, B>(state: State<S, A>, f: (x: A) => State<S, B>) => {
+  return (s: S): [S, B] => {
+    const [newState, res] = state(s);
+    return f(res)(newState);
+  };
+};
