0ce3d5348538340c868ea9af9d7e36d583e3ccf5
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-03-21 22:10:17 +0200

    finished first task on part 3

--- src/part3/find.ts
+++ src/part3/find.ts
@@ -2,24 +2,38 @@ import { Result, makeFailure, makeOk, bind, either } from "../lib/result";
 
 /* Library code */
 const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
-    for (let i = 0; i < a.length; i++) {
-        if (pred(a[i])) return a[i];
-    }
-    throw "No element found.";
-}
+  for (let i = 0; i < a.length; i++) {
+    if (pred(a[i])) return a[i];
+  }
+  throw "No element found.";
+};
 
-export const findResult = undefined;
+export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => {
+  const res = a.filter(pred);
+  if (res.length > 0) return makeOk(res[0]);
+  return makeFailure("");
+};
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
-    try {
-        const x = findOrThrow(x => x % 2 === 0, a);
-        return x * x;
-    } catch (e) {
-        return -1;
-    }
+  try {
+    const x = findOrThrow((x) => x % 2 === 0, a);
+    return x * x;
+  } catch (e) {
+    return -1;
+  }
+};
+
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => {
+  return bind(
+    findResult((x) => x % 2 === 0, a),
+    (x: number) => makeOk(x * x)
+  );
+};
+
+export const returnSquaredIfFoundEven_v3 = (a: number[]): number => {
+    return either(findResult((x) => x % 2 === 0, a),
+    (x: number) => x*x,
+    (message:string)=> -1
+    )
 }
-
-export const returnSquaredIfFoundEven_v2 = undefined;
-
-export const returnSquaredIfFoundEven_v3 = undefined;
