1dd056139247ee6fb9ca9c13762af996ae782326
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-03-22 01:48:38 +0200

    Major work on enqueue

--- src/part3/queue.ts
+++ src/part3/queue.ts
@@ -1,9 +1,25 @@
 import { State, bind } from "./state";
+import * as R from "ramda";
 
 export type Queue = number[];
 
-export const enqueue = undefined;
+export const enqueue: (x: number) => State<Queue, number> = (x: number) => {
+  return (init: Queue) => [init.concat([x]), x];
+};
 
-export const dequeue = undefined;
+export const dequeue: State<Queue, number> = (init: Queue) => [
+  R.slice(1, Infinity, init),
+  init[0],
+];
 
-export const queueManip = undefined;
+export const queueManip: State<Queue, number> = (init: Queue) => {
+  const [s1, x] = dequeue(init);
+  const [s2, y] = enqueue(x * 2)(s1);
+  const [s3, res3] = enqueue(x / 3)(s2);
+  const [s4, z] = dequeue(s3);
+  return [s4, z];
+};
+
+
+console.log("before: [6,7,8]");
+console.log(queueManip([6, 7, 8]));
