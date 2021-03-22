3536a49691eb57d03ae4fd07e9fa1c08c4d2816c
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-03-22 02:25:40 +0200

    Improved Queue

--- src/part3/queue.ts
+++ src/part3/queue.ts
@@ -3,8 +3,8 @@ import * as R from "ramda";
 
 export type Queue = number[];
 
-export const enqueue: (x: number) => State<Queue, number> = (x: number) => {
-  return (init: Queue) => [init.concat([x]), x];
+export const enqueue: (x: number) => State<Queue, any> = (x: number) => {
+  return (init: Queue) => [init.concat([x]), undefined];
 };
 
 export const dequeue: State<Queue, number> = (init: Queue) => [
@@ -12,14 +12,23 @@ export const dequeue: State<Queue, number> = (init: Queue) => [
   init[0],
 ];
 
-export const queueManip: State<Queue, number> = (init: Queue) => {
-  const [s1, x] = dequeue(init);
-  const [s2, y] = enqueue(x * 2)(s1);
-  const [s3, res3] = enqueue(x / 3)(s2);
-  const [s4, z] = dequeue(s3);
-  return [s4, z];
-};
+// export const queueManip: State<Queue, number> = (init: Queue) => {
+//   //   const [s1, x] = dequeue(init);
+//   //   const [s2, y] = enqueue(x * 2)(s1);
+//   //   const [s3, res3] = enqueue(x / 3)(s2);
+//   //   const [s4, z] = dequeue(s3);
+//   //   return [s4, z];
 
+// //   const b1 = bind(dequeue, (x: number) => enqueue(x * 2));
+// //   const b2 = bind(b1, (x: number) => enqueue(x / 3));
+// };
+
+export const queueManip: State<Queue, number> = bind(
+  bind(dequeue, (x: number) =>
+    bind(enqueue(x * 2), (y: number) => enqueue(x / 3))
+  ),
+  (z: number) => dequeue
+);
 
 console.log("before: [6,7,8]");
 console.log(queueManip([6, 7, 8]));
