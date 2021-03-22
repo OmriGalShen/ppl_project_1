f9c3782e006702cede6fdfec3c5635bab6fcaf50
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-03-22 02:50:53 +0200

    Overall fix

--- src/part3/stack.ts
+++ src/part3/stack.ts
@@ -1,9 +1,17 @@
 import { State, bind } from "./state";
+import * as R from "ramda";
 
 export type Stack = number[];
 
-export const push = undefined;
+export const push: (x: number) => State<Stack, any> = (x: number) => {
+  return (init: Stack) => [[x].concat(init), undefined];
+};
 
-export const pop = undefined;
+export const pop: State<Stack, number> = (init: Stack) => [
+  R.tail(init),
+  init[0],
+];
 
-export const stackManip = undefined;
+export const stackManip: State<Stack, number> = bind(pop, (x: number) =>
+  bind(push(x * x), (a: any) => bind(pop, (y: number) => push(x + y)))
+);
