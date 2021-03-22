f3590f963b938d66b869d91cb957a09307f3fe5b
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-03-22 02:50:39 +0200

    fixed queueManip

--- src/part3/queue.ts
+++ src/part3/queue.ts
@@ -12,23 +12,6 @@ export const dequeue: State<Queue, number> = (init: Queue) => [
   init[0],
 ];
 
-// export const queueManip: State<Queue, number> = (init: Queue) => {
-//   //   const [s1, x] = dequeue(init);
-//   //   const [s2, y] = enqueue(x * 2)(s1);
-//   //   const [s3, res3] = enqueue(x / 3)(s2);
-//   //   const [s4, z] = dequeue(s3);
-//   //   return [s4, z];
-
-// //   const b1 = bind(dequeue, (x: number) => enqueue(x * 2));
-// //   const b2 = bind(b1, (x: number) => enqueue(x / 3));
-// };
-
-export const queueManip: State<Queue, number> = bind(
-  bind(dequeue, (x: number) =>
-    bind(enqueue(x * 2), (y: number) => enqueue(x / 3))
-  ),
-  (z: number) => dequeue
+export const queueManip: State<Queue, number> = bind(dequeue, (x: number) =>
+  bind(enqueue(2 * x), (y: any) => bind(enqueue(x / 3), (z: any) => dequeue))
 );
-
-console.log("before: [6,7,8]");
-console.log(queueManip([6, 7, 8]));
